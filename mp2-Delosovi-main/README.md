9ad7d4d0df99c66b3b7260ba70f11016ed312381
Ovis Del <124535584+Delosovi@users.noreply.github.com>
2023-07-10 13:13:58 -0400

    Add files via upload
    Mini project 2

---
+++ mp2-Delosovi-main/README.md
@@ -0,0 +1,5 @@
+# mp2-Delosovi
+
+https://www.youtube.com/watch?v=jVA_qgjMmuA
+
+mp2-Delosovi created by GitHub Classroom
---
+++ mp2-Delosovi-main/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.scorecounterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    private TextView Title = null;
+    private TextView Display = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+
+        }
+
+    }
